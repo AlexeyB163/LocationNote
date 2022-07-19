61a316b2eabde55c99feb5bdf76a9a86a337b1aa
AlexeyB163 <sourceaisa@mail.ru>
2022-07-19 11:39:12 +0400

    add tabBarController

---
+++ LocationNote/Base/TabBarController.swift
@@ -0,0 +1,45 @@
+//
+//  TabBarViewController.swift
+//  LocationNote
+//
+//  Created by User on 19.07.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listVC = ListOfNotesViewController()
+        let detailsVC = NoteDetailsViewController()
+        
+        let configureItem = UIImage.SymbolConfiguration(weight: .semibold)
+        let imageItem = UIImage(systemName: "square.fill", withConfiguration: configureItem)
+        
+        viewControllers = [
+            setupNavigationController(
+                rootViewController: listVC,
+                title: "Notes",
+                image: imageItem ?? UIImage()),
+            setupNavigationController(
+                rootViewController: detailsVC,
+                title: "Notes",
+                image: imageItem ?? UIImage())
+        
+        ]
+        
+    }
+    
+    private func setupNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        
+        
+        return navigationVC
+    }
+    
+    
+    
+}
--- LocationNote/NoteDetails/NoteDetailsViewController.swift
+++ LocationNote/NoteDetails/NoteDetailsViewController.swift
@@ -21,7 +21,40 @@ class NoteDetailsViewController: UIViewController {
     
     var presenter: NoteDetailsViewOutputProtocol!
     var configurator: NoteDetailsConfiguratorInputProtocol = NoteDetailsConfigurator()
+    
+    let textView = UITextView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurator.configure(with: self)
+        setupTextView()
+        setupConstraits()
+    }
 }
+
+// MARK: - Setup TextView
+extension NoteDetailsViewController {
+    
+    private func setupTextView() {
+        view.addSubview(textView)
+        textView.backgroundColor = .cyan
+    }
+    
+    private func setupConstraits() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+}
+
+// MARK: - Setup NavigationBar
+extension NoteDetailsViewController {
+    
+}
+
 
 extension NoteDetailsViewController: NoteDetailsViewInputProtocol {
     
